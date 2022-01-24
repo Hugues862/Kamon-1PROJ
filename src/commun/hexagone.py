8dfb863943172e42c982164f111e54f42ee6135d
Hugues862 <huguesstock@gmail.com>
2022-01-24 16:51:00 +0100

    mod functions

--- src/commun/hexagone.py
+++ src/commun/hexagone.py
@@ -1,19 +1,19 @@
 
 class hexa():
-    
-    def __init__(self, set, theme = "original", side = None) -> None:
-        
+
+    def __init__(self, set, theme="original", side=None):
+
         self.__state = set
         self.changeColor()
         self.__side = side
         self.__border = "black"
         self.setImage(theme)
-        
+
         self.__last = False
         self.__selected = False
         self.__player = 0
-    
-    def getState(self) -> int:
+
+    def getState(self):
         """
         Gets the state of the Hex cell.
 
@@ -21,8 +21,8 @@ class hexa():
                 int: Value of the state.
         """
         return self.__state
-    
-    def getColor(self) -> str:
+
+    def getColor(self):
         """
         Gets the color of the cell.
 
@@ -30,8 +30,8 @@ class hexa():
                 str: String of the Hex color of the cell.
         """
         return self.__color
-    
-    def getBorder(self) -> str:
+
+    def getBorder(self):
         """
         Gets the color of the cell's border.
 
@@ -39,8 +39,8 @@ class hexa():
                 str: String of the Hex color of the cell's border.
         """
         return self.__color
-    
-    def getImage(self) -> str:
+
+    def getImage(self):
         """
         Gets the path of the image of this cell.
 
@@ -48,68 +48,68 @@ class hexa():
                 str: String of a path to the asset.
         """
         return self.__src
-    
-    def changeColor(self) -> None:
-        
+
+    def changeColor(self):
+
         tmp = self.__state
-        
+
         if tmp == 0:
             self.__color = "white"
-            
+
         if tmp == -1:
             self.__color = "black"
-            
+
         if tmp > 0 and tmp <= 6:
             self.__color = "color1"
-            
+
         if tmp > 6 and tmp <= 12:
             self.__color = "color2"
-            
+
         if tmp > 12 and tmp <= 18:
             self.__color = "color3"
-            
+
         if tmp > 18 and tmp <= 24:
             self.__color = "4"
-            
+
         if tmp > 24 and tmp <= 30:
             self.__color = "5"
-            
+
         if tmp > 30 and tmp <= 36:
             self.__color = "6"
-            
-    def setImage(self, theme: str) -> None:
-        
+
+    def setImage(self, theme: str):
+
         tmp = self.__state
-        
+
         if tmp == 0 or tmp == -1:
             return
-        
+
         if tmp - 1 % 6 == 0:
-            
+
             self.__src = '../assets/' + theme + '/0.png'
-        
+
         if tmp - 1 % 6 == 1:
-            
+
             self.__src = '../assets/' + theme + '/1.png'
-            
+
         if tmp - 1 % 6 == 2:
-            
+
             self.__src = '../assets/' + theme + '/2.png'
-            
+
         if tmp - 1 % 6 == 3:
-            
+
             self.__src = '../assets/' + theme + '/3.png'
-            
+
         if tmp - 1 % 6 == 4:
-            
+
             self.__src = '../assets/' + theme + '/4.png'
-            
+
         if tmp - 1 % 6 == 5:
-            
+
             self.__src = '../assets/' + theme + '/5.png'
-            
+
     def changeBorder(self):
-        
+
         if self.__last:
             self.__border = "Red"
 
@@ -118,5 +118,3 @@ class hexa():
 
         elif self.__player == 2:
             self.__border = "Green"
-            
-    
