35a5cf28e21da70edcda935d78ab91881d60ae57
Yoann Chhang <yoann.chhang@gmail.com>
2022-03-20 16:21:41 +0100

    Correction on Side, opposite side with number

--- src/commun/table.py
+++ src/commun/table.py
@@ -52,35 +52,37 @@ class Table():
 
                 if i == 0:
                     if j == 0:
-                        side = "G&Y"
+                        side = "G1&Y1"
                     elif j == 7 - abs(3 - i) - 1:
-                        side = "G&B"
+                        side = "G1&B1"
                     else:
-                        side = "G"
+                        side = "G1"
 
                 elif i == 6:
                     if j == 0:
-                        side = "G&B"
+                        side = "G2&B2"
                     elif j == 7 - abs(3 - i) - 1:
-                        side = "G&Y"
+                        side = "G2&Y2"
                     else:
-                        side = "G"
+                        side = "G2"
 
                 elif i == 1 or i == 2:
                     if j == 0:
-                        side = "Y"
+                        side = "Y1"
                     elif j == 7 - abs(3 - i) - 1:
-                        side = "B"
+                        side = "B1"
 
                 elif i == 4 or i == 5:
                     if j == 0:
-                        side = "B"
+                        side = "B2"
                     elif j == 7 - abs(3 - i) - 1:
-                        side = "Y"
+                        side = "Y2"
 
                 elif i == 3:
-                    if j == 0 or j == 7 - abs(3 - i) - 1:
-                        side = "B&Y"
+                    if j == 0: 
+                        side = "B2&Y1"
+                    elif j == 7 - abs(3 - i) - 1:
+                        side = "B1&Y2"
 
                 tmp.append(hexa(test, theme, side))
                 tmp.append(hexa(0))
@@ -117,22 +119,7 @@ class Table():
         return res
     
     def printSide(self):
-        """Prints a pretty version of the grid, instead of having :
-        [[0, 0, 0, -1, 0, 16, 0, 14, 0, 33, 0, 0, 0, 0], [0, 0, 21, 0, 32, 0, 23, 0, 28, 0, 30, 0, 0, 0], ...]
-
-        we have
-
-                0   1   2   3   4   5   6   7   8   9   10  11 12 13
-        0              -1      16      14      33              
-        1          21      32      23      28      30          
-        2      13      17      31      19       3       7      
-        3  35      20       2      25       9      11      8   
-        4      27      34      22      26      18      29      
-        5           4       1      36       5      24          
-        6              15      12       6      10               
-
-        Returns:
-            [pd.DataFrame]: dataframe version of self.__grid, 0 replaced with empty str
+        """
         """
 
         tmp = self.getGridSide()
@@ -141,5 +128,5 @@ class Table():
         return res
     
 t = Table()
-t.printGrid()
+t.printSide()
 
