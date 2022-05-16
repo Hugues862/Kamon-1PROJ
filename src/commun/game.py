ddaa05f142195bd524fe8cb6d84372d32c0dab6e
Yoann Chhang <yoann.chhang@gmail.com>
2022-05-16 09:46:29 +0200

    Last Coord Fix

--- src/commun/game.py
+++ src/commun/game.py
@@ -23,39 +23,39 @@ class Game:
         
         grid = self.__table.getGrid()
         
-        xLast, yLast = self.__table.getLastCoord()
+        if self.__table.getLastCoord() != None:
+            lastX, lastY = self.__table.getLastCoord()
         
-        if grid[y][x].getPlayer() == 0:
-            if not grid[y][x].getSelected():
-                grid[y][x].setSelected()
-                
+        # if grid[y][x].getPlayer() == 0:
+        #     if not grid[y][x].getSelected():
+        #         grid[y][x].setSelected()       
         
         if grid[y][x].getPlayer() == 0:
             if not grid[y][x].getSelected():
                 grid[y][x].setSelected()
 
                 if self.__table.getSelectedCoord() != None:
-                    lastX, lastY = self.__table.getSelectedCoord()
-                    grid[lastY][lastX].setSelected()
+                    selectX, selectY = self.__table.getSelectedCoord()
+                    grid[selectY][selectX].setSelected()
                 self.__table.setSelectedCoord(x, y)
                 
             elif grid[y][x].getSelected():
                 
-                if grid[yLast][xLast].color() == grid[y][x].color() or grid[yLast][xLast].getImage() == grid[y][x].getImage():
+                if self.__table.getLastCoord() == None or grid[lastY][lastX].getColor() == grid[y][x].getColor() or grid[lastY][lastX].getImage() == grid[y][x].getImage():
                     grid[y][x].setSelected()
                     grid[y][x].setLast()
                     grid[y][x].setPlayer(self.__turn + 1)
                     
                     if self.__table.getLastCoord() != None :
-                        lastX, lastY = self.__table.getLastCoord()
-                        grid[lastY][lastX].setLast()
-                        self.__table.setLastCoord(x, y)
+                        grid[lastY][lastX].setLast() # Removes Lest status from last ring
+                        
+                    self.__table.setLastCoord(x, y)
                 
     def turnChange(self):
         self.__turn = (self.__turn + 1)  % 2
     
     def win(self):
-        pass       
+        pass
             
     def checkWin(self, x, y):
         
@@ -66,13 +66,13 @@ class Game:
         if self.__turn== 0  or self.__turn == 1 :
 
             if ("G1" or "G1&Y1" or "G1&B1") and ("G2" or "G2&Y2" or "G2&B2") in sides:
-                win()
+                self.win()
 
             elif ("Y1" or "B2&Y1" or "G1&Y1") and ("Y2" or "B1&Y2" or "G2&Y2") in sides:
-                win()
+                self.win()
 
             elif ("B1" or "G1&B1" or"G1&Y1") and ("B2" or "B2&Y1" or "G2&B2") in sides:
-                win()
+                self.win()
             
         #Check if there a un boucle for the condition of win 
         # for self.checkNeighbors() in range(self.__table.getGrid()[y][x]):
@@ -89,12 +89,5 @@ class Game:
                 self.__table.setLastCoord(x, y)
 
             
-        
-
-        
-                
-            
-            
-
 def createGame():
     return Game()
