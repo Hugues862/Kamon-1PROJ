4f595b3b4d862d172d0737d8fe39b8f3101aab32
Noah DIJOUX <noahdijoux@gmail.com>
2022-06-12 23:44:24 +0400

    Menu ok manque bouton et commande associé

--- src/main.py
+++ src/main.py
@@ -1,137 +1,180 @@
-# ICI LE MENU
-import game
+import commun.game
 from tkinter import *
 from PIL import ImageTk, Image
 
 
-class Menus:
-    w = Tk()
-    w.geometry("900x500")
-    w.configure(bg="#262626")
-    w.resizable(0, 0)
-    w.title("KAMON")
+w = Tk()
+w.geometry("900x500")
+w.configure(bg="#262626")
+w.resizable(0, 0)
+w.title("KAMON")
 
-    def default_home():
-        frame2 = Frame(w, width=900, height=455, bg="#262626")
-        frame2.place(x=0, y=45)
-        frame3 = Frame(w, width=900, height=455, bg="#262626")
-        frame3.place(x=0, y=50)
-        label2 = Label(
-            frame2, text="Bienvenue sur le jeu KAMON", fg="white", bg="#262626"
-        )
-        label2.config(font=("Comic Sans MS", 50))
-        label2.place(x=290, y=150 - 45)
-        label3 = Label(
-            frame3, text="Veuillez déroulez le menu", fg="white", bg="#262626"
-        )
-        label3.config(font=("Comic Sans MS", 20))
-        label3.place(x=290, y=150 - 45)
 
-    def singleplayer():
-        frame1.destroy()
-        frame2 = Frame(w, width=900, height=455, bg="#262626")
-        frame2.place(x=0, y=45)
-        label2 = Label(frame2, text="SinglePlayer", fg="white", bg="#262626")
-        label2.config(font=("Comic Sans MS", 90))
-        label2.place(x=290, y=150 - 45)
-        toggle_win()
+def default_home():
+    f2 = Frame(w, width=900, height=455, bg="#262626")
+    f2.place(x=0, y=45)
+    l2 = Label(f2, text="BIENVENUE SUR LE JEU", fg="#8A2BE2", bg="#262626")
+    l2.config(font=("Comic Sans MS", 50))
+    l2.place(x=20, y=150 - 80)
+    f3 = Frame(w, width=900, height=455, bg="#262626")
+    f3.place(x=0, y=200)
+    l3 = Label(f3, text="KAMON", fg="white", bg="#262626")
+    l3.config(font=("Comic Sans MS", 50))
+    l3.place(x=250, y=1)
 
-    def multiPlayer():
-        frame1.destroy()
-        frame2 = Frame(w, width=900, height=455, bg="white")
-        frame2.place(x=0, y=45)
-        label2 = Label(frame2, text="MultiPlayer", fg="black", bg="white")
-        label2.config(font=("Comic Sans MS", 90))
-        label2.place(x=290, y=150 - 45)
-        toggle_win()
 
-    def option():
-        frame1.destroy()
-        f2 = Frame(w, width=900, height=455, bg="white")
-        f2.place(x=0, y=45)
-        l2 = Label(f2, text="Option", fg="black", bg="white")
-        l2.config(font=("Comic Sans MS", 90))
-        l2.place(x=320, y=150 - 45)
-        toggle_win()
+def singleplayer():
+    f1.destroy()
+    f2 = Frame(w, width=900, height=455, bg="#262626")
+    f2.place(x=0, y=45)
+    l2 = Label(f2, text="YOU CHOOSE SINGLEPLAYER MODE ", fg="#8A2BE2", bg="#262626")
+    l2.config(font=("Comic Sans MS", 35))
+    l2.place(x=40, y=150 - 45)
+    bouton2 = Button(w, text="CLICK TO PLAY ", font=20)
+    bouton2.pack(side="right", ipadx=20, padx=30, pady=20)
+    bouton2.place(x=350, y=400)
 
-    def toggle_win():
-        global frame1
-        frame1 = Frame(w, width=300, height=500, bg="#12c4c0")
-        frame1.place(x=0, y=0)
+    # Player's name label
 
-        # buttons
-        def bttn(x, y, text, bcolor, fcolor, cmd):
-            def entrer(e):
-                myButton1["background"] = bcolor  # ffcc66
-                myButton1["foreground"] = "#262626"  # 000d33
-
-            def sortir(e):
-                myButton1["background"] = fcolor
-                myButton1["foreground"] = "#262626"
-
-            myButton1 = Button(
-                frame1,
-                text=text,
-                width=42,
-                height=2,
-                fg="#262626",
-                border=0,
-                bg=fcolor,
-                activeforeground="#262626",
-                activebackground=bcolor,
-                command=cmd,
-            )
-
-            myButton1.bind("<Enter>", entrer)
-            myButton1.bind("<Leave>", sortir)
-
-            myButton1.place(x=x, y=y)
-
-        bttn(0, 80, "Singleplayer", "#0f9d9a", "#12c4c0", singleplayer)
-        bttn(0, 117, "MultiPlayer", "#0f9d9a", "#12c4c0", multiPlayer)
-        bttn(0, 154, "Option", "#0f9d9a", "#12c4c0", option)
-
-        #
-        def dele():
-            frame1.destroy()
-            b2 = Button(
-                w,
-                image=img1,
-                command=toggle_win,
-                border=0,
-                bg="#262626",
-                activebackground="#262626",
-            )
-            b2.place(x=5, y=8)
-
-        global img2
-        img2 = ImageTk.PhotoImage(Image.open("close.png"))
-
-        Button(
-            frame1,
-            image=img2,
-            border=0,
-            command=dele,
-            bg="#12c4c0",
-            activebackground="#12c4c0",
-        ).place(x=5, y=10)
-
-    default_home()
-
-    img1 = ImageTk.PhotoImage(Image.open("open.png"))
-
-    global b2
-    b2 = Button(
-        w,
-        image=img1,
-        command=toggle_win,
-        border=0,
-        bg="#262626",
-        activebackground="#262626",
+    # Player 1
+    cadre1 = LabelFrame(
+        w, text="Joueur 1", font=("Comic Sans MS", 30), bg="#262626", fg="#8A2BE2"
     )
-    b2.place(x=5, y=8)
+    cadre1.pack(padx=5, pady=5)
+    Label1 = Label(
+        cadre1, text=" Pseudo :", fg="red", bg="yellow", font=("Comic Sans MS", 12)
+    )
+    cadre1.place(x=100, y=250)
+    Label1.pack(padx=10, pady=10, side=LEFT)
+    E1 = Entry(cadre1)
+    E1.pack(padx=5, pady=5, side=LEFT)
 
-    def singlePlayerMode(self):
+    # Player 2
+    cadre2 = LabelFrame(
+        w, text="Joueur 2", font=("Comic Sans MS", 30), bg="#262626", fg="#8A2BE2"
+    )
+    cadre2.pack(padx=5, pady=5)
+    Label2 = Label(
+        cadre2, text="Pseudo :", fg="black", bg="yellow", font=("Comic Sans MS", 12)
+    )
+    cadre2.place(x=500, y=250)
+    Label2.pack(padx=10, pady=10, side=LEFT)
+    E2 = Entry(cadre2)
+    E2.pack(padx=5, pady=5, side=LEFT)
+    toggle_win()
+
+
+def multiplayer():
+    f1.destroy()
+    f2 = Frame(w, width=900, height=455, bg="white")
+    f2.place(x=0, y=45)
+    l2 = Label(f2, text="Multiplayer", fg="black", bg="white")
+    l2.config(font=("Comic Sans MS", 90))
+    l2.place(x=10, y=150 - 45)
+    toggle_win()
+
+
+def option():
+    f1.destroy()
+    f2 = Frame(w, width=900, height=455, bg="white")
+    f2.place(x=0, y=45)
+    l2 = Label(f2, text="Option", fg="black", bg="white")
+    l2.config(font=("Comic Sans MS", 90))
+    l2.place(x=10, y=150 - 45)
+    toggle_win()
+
+
+def toggle_win():
+    global f1
+    f1 = Frame(w, width=300, height=500, bg="#8A2BE2")
+    f1.place(x=0, y=0)
+
+    # buttons
+    def bttn(x, y, text, bcolor, fcolor, cmd):
+        def on_entera(e):
+            myButton1["background"] = bcolor  # ffcc66
+            myButton1["foreground"] = "#262626"  # 000d33
+
+        def on_leavea(e):
+            myButton1["background"] = fcolor
+            myButton1["foreground"] = "#262626"
+
+        myButton1 = Button(
+            f1,
+            text=text,
+            width=42,
+            height=2,
+            fg="#262626",
+            border=0,
+            bg=fcolor,
+            activeforeground="#262626",
+            activebackground=bcolor,
+            command=cmd,
+        )
+
+        myButton1.bind("<Enter>", on_entera)
+        myButton1.bind("<Leave>", on_leavea)
+
+        myButton1.place(x=x, y=y)
+
+    bttn(0, 50, "H O M E", "#FFFAF0", "#8A2BE2", default_home)
+    bttn(0, 80, "S I N G L E P L A Y E R", "#FFFAF0", "#8A2BE2", singleplayer)
+    bttn(0, 117, "M U L T I P L A Y E R", "#FFFAF0", "#8A2BE2", multiplayer)
+    bttn(0, 154, "O P T I O N", "#FFFAF0", "#8A2BE2", option)
+
+    #
+    def dele():
+        f1.destroy()
+        b2 = Button(
+            w,
+            image=img1,
+            command=toggle_win,
+            border=0,
+            bg="#8A2BE2",
+            activebackground="#FFFAF0",
+        )
+        b2.place(x=5, y=8)
+
+    global img2
+    img2 = ImageTk.PhotoImage(Image.open("close.png"))
+
+    Button(
+        f1, image=img2, border=0, command=dele, bg="#8A2BE2", activebackground="#8A2BE2"
+    ).place(x=5, y=10)
+
+
+default_home()
+
+img1 = ImageTk.PhotoImage(Image.open("open.png"))
+
+global b2
+b2 = Button(
+    w,
+    image=img1,
+    command=toggle_win,
+    border=0,
+    bg="#8A2BE2",
+    activebackground="#262626",
+)
+b2.place(x=5, y=8)
+
+
+w.mainloop()
+
+
+"""def singlePlayerMode():
+    w.destroy()
+    commun.game.gameRun("v1")
+
+
+def multiPlayerMode():
+    w.destroy()
+    commun.game.gameRun("v1")
+
+def menuRun():
+    start = Menus()"""
+
+"""def singlePlayerMode(self):
         self.window.destroy()
         game.gameRun("v1")
 
@@ -145,7 +188,7 @@ class Menus:
     w.mainloop()
 
 
-""" Init some frame for the contents
+ Init some frame for the contents
         frame1 = Frame(self.window)
         frame2 = Frame(self.window)
         frame3 = Frame(self.window)
@@ -249,4 +292,4 @@ class Menus:
 
 if __name__ == "__main__":
     menuRun()
-"""
+    """
